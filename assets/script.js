027529c2c4df38de2b56fe8d60eab68750d4ad4f
Robert Baufeld <rbaufeld@posteo.de>
2024-07-31 11:41:51 +0200

    added hover effect for navbar and fixed text centering in hero section

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,14 @@
-const navItems = document.querySelectorAll('.nav-link');
-const arrows = document.querySelectorAll('.arrow');
+const navLinks = document.querySelectorAll('.nav-link');
 
-navItems.forEach(item => {
+navLinks.forEach(item => {
   item.addEventListener('click', () => {
-    navItems.forEach(i => {
+    navLinks.forEach(i => {
       i.classList.remove('fw-bold');
       i.textContent = i.textContent.replace(/_$/, '');
     });
 
     item.classList.add('fw-bold');
     item.textContent += '_';
-  });
-});
-
-arrows.forEach(arrow => {
-  arrow.addEventListener('click', () => {
-    const targetSectionId = arrow.getAttribute('href').substring(1);
-    const targetNavItem = document.querySelector(`a[href="#${targetSectionId}"]`);
-
-    navItems.forEach(item => item.classList.remove('fw-bold'));
-    targetNavItem.classList.add('fw-bold');
   });
 });
 
